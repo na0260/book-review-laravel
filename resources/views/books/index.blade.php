51a8d2e801afdf5858aab8742b2f543d3c08d437
na0260 <samjr.008@gmail.com>
2024-03-15 02:08:13 +0600

    index ui added

---
+++ resources/views/books/index.blade.php
@@ -0,0 +1,42 @@
+@extends('layouts.app')
+
+@section('content')
+    <h1 class="text-3xl font-semibold text-slate-800 mb-4">Books</h1>
+
+    <form method="GET" action="{{ route('books.index') }}">
+        <input type="text" name="title" placeholder="Search by title" value="{{request('title')}}" class="input">
+        <button type="submit" class="btn">Search</button>
+        <a href="{{route('books.index')}}" class="btn">Clear</a>
+    </form>
+
+    <ul>
+        @forelse($books as $book)
+            <li class="mb-4">
+                <div class="book-item">
+                    <div
+                        class="flex flex-wrap items-center justify-between">
+                        <div class="w-full flex-grow sm:w-auto">
+                            <a href="{{route('books.show',$book)}}" class="book-title">{{$book->title}}</a>
+                            <span class="book-author">by {{$book->author}}</span>
+                        </div>
+                        <div>
+                            <div class="book-rating">
+                                {{ number_format($book->review_avg_rating,1) }}
+                            </div>
+                            <div class="book-review-count">
+                                out of {{$book->reviews_count}} {{Str::plural('review',$book->reviews_count)}}
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </li>
+        @empty
+            <li class="mb-4">
+                <div class="empty-book-item">
+                    <p class="empty-text">No books found</p>
+                    <a href="{{route('books.index')}}" class="reset-link">Reset criteria</a>
+                </div>
+            </li>
+        @endforelse
+    </ul>
+@endsection
