3135b32470e2e179b1ba5c9798d9fbbaff41311d
na0260 <samjr.008@gmail.com>
2024-03-15 01:42:03 +0600

    query builder added for popular, highestRated and min reviews books

--- app/Models/Book.php
+++ app/Models/Book.php
@@ -2,6 +2,7 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Builder;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
@@ -13,5 +14,36 @@ class Book extends Model
     public function reviews()
     {
         return $this->hasMany(Review::class);
+    }
+
+    public function scopePopular(Builder $query, $from = null, $to = null): Builder
+    {
+        return $query->withCount([
+            'reviews'=> fn(Builder $q) => $this->dateRangeFilter($q, $from, $to)
+        ])
+            ->orderBy('reviews_count', 'desc');
+    }
+
+    public function scopeHighestRated(Builder $query, $from = null, $to = null): Builder
+    {
+        return $query->withAvg([
+            'reviews'=> fn(Builder $q) => $this->dateRangeFilter($q, $from, $to)
+        ], 'rating')
+            ->orderBy('reviews_avg_rating', 'desc');
+    }
+
+    public function scopeMinReviews(Builder $query, int $min): Builder
+    {
+        return $query->having('reviews_count', '>=', $min);
+    }
+
+    private function dateRangeFilter(Builder $q, $from = null, $to = null){
+        if ($from && !$to) {
+            $q->where('created_at', '>=', $from);
+        }elseif (!$from && $to) {
+            $q->where('created_at', '<=', $to);
+        }elseif ($from && $to) {
+            $q->whereBetween('created_at', [$from, $to]);
+        }
     }
 }
