fff2cc30f7de0b5a52c76ccbc629d1ecb0491096
na0260 <samjr.008@gmail.com>
2024-03-15 17:02:50 +0600

    filter query added to controller

--- app/Http/Controllers/BookController.php
+++ app/Http/Controllers/BookController.php
@@ -13,11 +13,19 @@ class BookController extends Controller
     public function index(Request $request)
     {
         $title = $request->input('title');
+        $filter = $request->input('filter');
         $books = Book::when(
             $title,
-            fn($query, $title) => $query->title($title))
-            ->get();
-
+            fn($query, $title) => $query->title($title)
+        );
+        $books = match ($filter){
+            'popular_last_month' => $books->popularLastMonth(),
+            'popular_last_6months' => $books->popularLast6Month(),
+            'top_rated_last_month' => $books->highestRatedLastMonth(),
+            'top_rated_last_6months' => $books->highestRatedLast6Month(),
+            default => $books
+        };
+        $books = $books->get();
         return view('books.index',['books' => $books]);
     }
 
