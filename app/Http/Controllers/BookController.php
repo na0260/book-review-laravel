bdea8f1d0e901300944d5cd664d3cb5111d132d5
na0260 <samjr.008@gmail.com>
2024-03-15 23:59:14 +0600

    caching added

--- app/Http/Controllers/BookController.php
+++ app/Http/Controllers/BookController.php
@@ -27,6 +27,8 @@ class BookController extends Controller
             default => $books
         };
         $books = $books->get();
+        $cacheKey = 'books:'.$filter.':'.$title;
+        $books = cache()->remember($cacheKey, 3600, fn() => $books->get());
         return view('books.index',['books' => $books]);
     }
 
